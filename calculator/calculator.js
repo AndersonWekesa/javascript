97456cb9bde04243ba6cbf904e884e5c37b4f792
AndersonWekesa <opiyoanderson@hotmail.com>
2022-06-20 20:05:35 +0300

    Javascript Calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,30 @@
+function addition(num1, num2)
+{
+    return num1 + num2;
+}
+
+function subtraction(num1, num2)
+{
+    return num1 - num2;
+}
+
+function multiplication(num1, num2)
+{
+    return num1 * num2;
+}
+
+function division(num1, num2)
+{
+    return num1 / num2;
+}
+
+
+//Test Functions
+
+let num1 = 100;
+let num2 = 5;
+
+console.log(addition(num1, num2));
+console.log(subtraction(num1, num2));
+console.log(multiplication(num1, num2));
+console.log(division(num1, num2));
