f8c9ddf8bfb9856e13022146ee00c1206ad97b13
AndersonWekesa <opiyoanderson@hotmail.com>
2022-06-21 00:39:10 +0300

    Updated

--- Intro/Intro.js
+++
@@ -1,3 +0,0 @@
-console.log("Name: Anderson Wekesa")
-console.log("Height: 168cm")
-console.log("Country: Kenya")
---
+++ Intro/index.js
@@ -0,0 +1,5 @@
+document.write("Name: Anderson Wekesa")
+document.write("<br>") //New Line
+document.write("Height: 168cm")
+document.write("<br>") //New Line
+document.write("Country: Kenya")
